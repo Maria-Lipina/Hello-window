452e5e77f02bf51364c83441c83a288360a77ba7
Maria Lipina <m.o.lip714@gmail.com>
2023-04-07 14:13:01 +0300

    доделала загрузку данных из файла на тестовых паролях (пока напрямую)

--- database/database.sql
+++ database/database.sql
@@ -20,17 +20,18 @@ SELECT COUNT(id) FROM users;
 
 DROP TABLE IF EXISTS passwords;
 CREATE TABLE passwords (
+u_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 u_password VARCHAR(8)
 );
 
-LOAD DATA INFILE "D:\\IT\\Java\\Sandbox\\Hello window\\database\\passGen.txt" INTO TABLE passwords;
+LOAD DATA INFILE "D:\\IT\\Java\\Sandbox\\Hello window\\database\\passGen.txt" INTO TABLE passwords (u_password);
 # Файл здесь C:\ProgramData\MySQL\MySQL Server 8.0\Data\jfx если не указан абсолютный путь к другой папке
 # Стандартно скрипты здесь C:\Users\Admin\AppData\Roaming\DBeaverData\workspace6\General\Scripts
 
-/*update table?*/
-INSERT INTO jfx.users (password_hash)
-SELECT *
-FROM passwords;
+
+UPDATE users
+SET password_hash = ( SELECT u_password FROM passwords WHERE users.id = passwords.u_id);
+
 
 
 
